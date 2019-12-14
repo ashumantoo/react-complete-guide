3a2815fc9ae6a215bd9248572b13bc98b7aa99a1
ashutosh <ashumantoo@gmail.com>
2019-12-14 15:13:05 +0530

    working of react with html

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
     <div className="App">
       <h4>React App</h4>
     </div>
+
+    //without nesting
+    // React.createElement('div', null, 'h1', 'Hello I am react app'),
+
+    // //html nesting div to the h1 tag
+    // React.createElement('div', null, React.createElement('h1', 'null', 'Hello I am react app')),
+
+    // //apply css class using className
+    // React.createElement('div', { className: 'App' }, React.createElement('h1', 'null', 'Hello I am react app'))
   );
 }
 
