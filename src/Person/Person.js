0dbd8cef35c8f84de5fc65637ceb6fab7555148b
ashutosh <ashumantoo@gmail.com>
2019-12-14 16:49:47 +0530

    adding Children on props to display content b/w opening and closing tag.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const person = (props) => {
-return <p>I am {props.name} and I am {props.age} year old.</p>;
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} year old.</p>
+            {/* This children on props will get the value which is 
+                defined between the opening and closing tag of the element */}
+            <p> {props.children} </p>
+        </div>
+    );
 }
 
 export default person;
