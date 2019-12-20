785b8c6a2d05cc1427c1c5bc2e39c556509d6d81
ashutosh <ashumantoo@gmail.com>
2019-12-20 20:04:03 +0530

    Rendering Adjecent JSX element using Auxiliary file.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux';
 import './Person.css';
 
 class Person extends Component {
 
+    // render() {
+    //     console.log('[Person.js] rendering ....');
+    //     return (
+    //         <div className="Person" >
+    //             <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} year old.</p>
+    //             <p> {this.props.children} </p>
+    //             <input type="text" onChange={this.props.changed} value={this.props.name} />
+    //         </div>
+    //     );
+    // }
     render() {
         console.log('[Person.js] rendering ....');
         return (
-            <div className="Person" >
+            <Aux>
                 <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} year old.</p>
                 <p> {this.props.children} </p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
         );
     }
-
 }
 
 export default Person;
