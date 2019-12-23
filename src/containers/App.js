75d7b5c580c2344afc6d281661a52f4e8132bed4
ashutosh <ashumantoo@gmail.com>
2019-12-23 16:19:19 +0530

    2nd method of creating higher order of componet added.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import WithClass from '../hoc/WithClass'
+import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
 import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux'
 // import Basic from './Basic/Basic';
 import ListAndConditional from '../components/Basic/Lists-Conditionals'
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        <Cockpit></Cockpit>
-        {/* <Basic></Basic> */}
-        <hr></hr>
-        <ListAndConditional></ListAndConditional>
-      </div>
+      // <div className="App" >
+      //   <Cockpit></Cockpit>
+      //   {/* <Basic></Basic> */}
+      //   <hr></hr>
+      //   <ListAndConditional></ListAndConditional>
+      // </div>
 
       //using withClass higher order Component approach to wrapping another component
       // <WithClass classes="App" >
@@ -21,8 +23,19 @@ class App extends Component {
       //   <hr></hr>
       //   <ListAndConditional></ListAndConditional>
       // </WithClass>
+
+      //using the 2nd HOC approach
+      <Aux className="App" >
+        <Cockpit></Cockpit>
+        {/* <Basic></Basic> */}
+        <hr></hr>
+        <ListAndConditional></ListAndConditional>
+      </Aux>
     );
   }
 }
 
-export default App;
+// export default App;
+
+//2nd HOC approach
+export default withClass(App, "App");
---
+++ src/hoc/withClass.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const withClass = (WrappedComponent, className) => {
+
+    //this is a function that returns a functional components with jsx body
+    //this is a second method of creating higher order components(HOC)
+    return props => (
+        <div className={className}>
+            <WrappedComponent />
+        </div>
+    );
+}
+
+
+export default withClass;
