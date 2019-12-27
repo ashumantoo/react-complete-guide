32cbb12a7bf4e9c9a167103895afeb4a5fc8f153
ashutosh <ashumantoo@gmail.com>
2019-12-27 16:32:34 +0530

    added get http request and transforming the data.

--- src/Http-Example/containers/Blog/Blog.js
+++ src/Http-Example/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import NewPost from '../../components/NewPost/NewPost';
@@ -7,13 +8,41 @@ import classes from './Blog.module.css';
 
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios
+            .get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res);
+                //the response give 100 post at a time, just display 4 out of 100
+                const posts = res.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Mantoo'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+
     render() {
+        const posts = this.state.posts.map(post => {            
+            return <Post key={post.id} title={post.title} author={post.author} />;
+        });
+
         return (
             <div>
                 <section className={classes.Posts}>
+                    {/* <Post />
                     <Post />
-                    <Post />
-                    <Post />
+                    <Post /> */}
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
