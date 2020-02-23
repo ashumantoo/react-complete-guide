ae77de44b9f12d4df718909ae3fa89799bfffa82
ashutosh <ashumantoo@gmail.com>
2020-02-23 07:40:26 +0530

    useEffect hook added for fetching the data from server

--- src/Hooks-example/components/Todo.js
+++ src/Hooks-example/components/Todo.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Todo = props => {
 
@@ -25,12 +26,56 @@ const Todo = props => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
 
+    //============================== useEffect() hook ==============================
+    // 1.useEffect() hook get executed immediately as soon as the components gets
+    //   loaded. that means we should say if you want to write a code that should
+    //   run immediately after the component load you should write it inside the 
+    //   useEffect() hook 
+    //==============================================================================
+
+    //we could use the piece of code which we want to execute as soon as the component
+    //load outside the useEffect() hook like this getting the data from server using
+    //axios but this is very bad practice 
+    //the problem with calling this here would be that we call it inside the render 
+    //life cycle that is bad for performance reason. React does lot of things behind
+    //the seen when it render the component to updates the dom 
+    //
+    // axios.get('FIREBASE_REALTIME_DATABASE_API_URL').then(res => {
+    //     console.log(res);
+    // }).catch(error => {
+    //     console.log(error);
+    // });
+    //================================================================================
+    useEffect(() => {
+        //fetching the todo list data from the server
+        axios.get('FIREBASE_REALTIME_DATABASE_API_URL').then(res => {
+            console.log(res);
+            const todoData = res.data;
+            const todos = [];
+            for (const key in todoData) {
+                todos.push({ id: key, name: todoData[key].name });
+            }
+            setTodoList(todos);
+        }).catch(error => {
+            console.log(error);
+        });
+    });
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
     }
 
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName));
+
+        //saving todoList data to the firebase server
+        axios
+            .post('FIREBASE_REALTIME_DATABASE_API_URL', { name: todoName })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     return (
