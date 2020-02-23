a0bdb59805924fc8be3de7410e0a8acf8d02795a
ashutosh <ashumantoo@gmail.com>
2020-02-23 07:03:57 +0530

    using multiple hook for separating the logic

--- src/Hooks-example/components/Todo.js
+++ src/Hooks-example/components/Todo.js
@@ -8,16 +8,29 @@ const Todo = props => {
     // 2.Second value is always a function to update the state with a new value
     //===========================================================================
 
+    //============================ Rules of the react hooks =====================
+    // 1. You Must only use all the react hooks at the top level of the component
+    //    function i.e the root level of the function body
+    // 2. You must call the useState directly in your top level function body. You
+    //    must not call usestate() hook inside any function or if statement or inside 
+    //    the loop or inside the function of the function or nested function
+    //===========================================================================
+
     // const inputState = useState('');
     // const inputChangeHandler = (event) => {
     //     inputState[1](event.target.value);
     // }
 
-    //Array Destructuring
+    //Array Destructuring and using multiple useState hook for separating the logic
     const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value);
+    }
+
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
     }
 
     return (
@@ -28,9 +41,11 @@ const Todo = props => {
                 onChange={inputChangeHandler}
                 value={todoName}
             />
-            <button type="button">Add</button>
+            <button type="button" onClick={todoAddHandler}>Add</button>
             <ul>
-
+                {todoList.map(todo => (
+                    <li key={todo}>{todo}</li>
+                ))}
             </ul>
         </React.Fragment>
     )
